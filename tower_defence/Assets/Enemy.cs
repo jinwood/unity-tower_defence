ea53827581dc2d72e0b4cfe0d5beea73f6026ae0
Julian Inwood <jinwood8@outlook.com>
2016-12-07 23:03:02 +0000

    Scripted enemy movement

---
+++ tower_defence/Assets/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	public float speed = 10f;
+
+	private Transform target;
+	private int waypointIndex = 0;
+
+	void Start(){
+		target = Waypoints.waypoints[0];
+	}
+
+	void Update(){
+		var dir = target.position - transform.position;
+		transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+		if(Vector3.Distance(transform.position, target.position) <= 0.2f){
+			GetNextWaypoint();
+		}
+	}
+
+	void GetNextWaypoint(){
+
+		if(waypointIndex >= Waypoints.waypoints.Length -1){
+			Destroy(gameObject);
+			return;
+		}
+
+		waypointIndex++;
+		target = Waypoints.waypoints[waypointIndex];
+	}
+
+}
